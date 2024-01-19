c1ab07b1bc7760ec48b0de2dc62cbfed5e58bbcc
Matt Hammond <mjhammond94@gmail.com>
2024-01-19 17:20:43 +0000

    Add Register and SignIn functionality to components

--- firebase-auth/src/views/Register.vue
+++ firebase-auth/src/views/Register.vue
@@ -1,3 +1,33 @@
 <template>
-  Registration page
+  <h1>Create account</h1>
+  <p><input type="text" placeholder="email" v-model="email"/></p>
+  <p><input type="password" placeholder="password" v-model="password"/></p>
+  <p><button @click="register">Submit</button></p>
+  <p><button @click="signInWithGoogle">Sign In With Google</button></p>
 </template>
+
+<script setup>
+  import { ref } from "vue";
+  import { useRouter } from "vue-router";
+  import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+  const email = ref("");
+  const password = ref("");
+  const router = useRouter();
+
+  const register = () => {
+    createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+      .then((data) => {
+        console.log("Successfully registered new user!")
+        console.log(auth.currentUser)
+        router.push('/feed');
+      })
+      .catch((error) => {
+        console.log(error.code);
+        alert(error.message);
+      })
+  };
+
+  const signInWithGoogle = () => {
+
+  };
+</script>
--- firebase-auth/src/views/SignIn.vue
+++ firebase-auth/src/views/SignIn.vue
@@ -1,3 +1,54 @@
 <template>
-  Sign In Page
+  <h1>Sign In</h1>
+  <p><input type="text" placeholder="email" v-model="email"/></p>
+  <p><input type="password" placeholder="password" v-model="password"/></p>
+  <p v-if="errMsg">{{ errMsg }}</p>
+  <p><button @click="logIn">Log In</button></p>
+  <p><button @click="logInWithGoogle">Log In With Google</button></p>
 </template>
+
+<script setup>
+  import { ref } from "vue";
+  import { useRouter } from "vue-router";
+  import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+  const email = ref("");
+  const password = ref("");
+  const errMsg = ref();
+  const router = useRouter();
+
+  const logIn = () => {
+    signInWithEmailAndPassword(getAuth(), email.value, password.value)
+      .then((data) => {
+        console.log("Successfully logged in!")
+        console.log(auth.currentUser)
+        router.push('/feed');
+      })
+      .catch((error) => {
+        console.log(error.code);
+        // Firebase auth error codes passed through error.code
+        // auth/invalid-email => If email is not valid
+        // auth/user-not-found => User not been created/email spelt wrong
+        // auth/wrong-password => Wrong password input
+        // auth/user-disabled => User been banned
+
+        switch (error.code) {
+          case "auth/invalid-email":
+            errMsg.value = "Invalid email";
+            break;
+          case "auth/user-not-found":
+            errMsg.value = "No account with that email has been located";
+            break;
+          case "auth/wrong-password":
+            errMsg.value = "Password is incorrect";
+            break;
+          default: 
+            errMsg.value = "Email or password was incorrect";
+            break;
+        }
+      });
+  };
+
+  const logInWithGoogle = () => {
+
+  };
+</script>
