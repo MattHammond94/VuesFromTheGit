c595ef79f9eac12aa1097934b161848e2e6d1bde
Matt Hammond <mjhammond94@gmail.com>
2024-01-19 16:50:20 +0000

    Installl firebase and add SDK

--- firebase-auth/src/main.js
+++ firebase-auth/src/main.js
@@ -1,11 +1,21 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import { initializeApp } from "firebase/app";
 
+// Styling:
+import './assets/main.css'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1FpXNk29UCTKqiB-MUXsfOMdg4Q7NxN8",
+  authDomain: "vue-auth-1-cf956.firebaseapp.com",
+  projectId: "vue-auth-1-cf956",
+  storageBucket: "vue-auth-1-cf956.appspot.com",
+  messagingSenderId: "195761474351",
+  appId: "1:195761474351:web:bfd2c714397bebf9bd2fda"
+};
+
+initializeApp(firebaseConfig);
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
